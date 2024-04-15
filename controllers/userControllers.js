b852b7e33fc89df35608c3ca763c0dfa73efb21e
efrenjr-dev <efrenjr.rodriguez@gmail.com>
2024-04-15 15:08:51 +0800

    validate duplicate user email

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -12,20 +12,26 @@ getAllUsersController = (req, res) => {
 registerUserController = (req, res) => {
     console.log(req.body);
 
-    let newUser = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        mobileNo: req.body.mobileNo,
-        email: req.body.email,
-        password: req.body.password,
-    });
+    User.findOne({ email: req.body.email })
+        .then((foundUser) => {
+            if (foundUser !== null && foundUser.email === req.body.email) {
+                return res.send("Duplicate Email Found!");
+            } else {
+                let newUser = new User({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    mobileNo: req.body.mobileNo,
+                    email: req.body.email,
+                    password: req.body.password,
+                });
 
-    newUser
-        .save()
-        .then((result) => res.send(result))
+                newUser
+                    .save()
+                    .then((result) => res.send(result))
+                    .catch((err) => res.send(err));
+            }
+        })
         .catch((err) => res.send(err));
 };
-
-
 
 module.exports = { getAllUsersController, registerUserController };
